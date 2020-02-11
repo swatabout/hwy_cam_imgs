08d2f54ebec0a021aec48de4f247af686845495a
swatabout <sierrafouche@gmail.com>
2020-02-11 14:16:10 -0500

    Update Jills_Retinanet.py

--- API/Jills_Retinanet.py
+++ API/Jills_Retinanet.py
@@ -1,7 +1,6 @@
 from imageai.Detection import ObjectDetection
 import os, numpy as np
 import pandas as pd
-from IPython.display import Image
 
 def detect_object(image):
 
@@ -21,9 +20,8 @@ def detect_object(image):
         boxes = []
         custom_objects = detector.CustomObjects(car=True, truck=True)
         detections = detector.detectCustomObjectsFromImage(input_image = image,
-                                                      output_image_path = os.getcwd(), # hopefully same path as the container
-                                                      custom_objects = custom_objects, minimum_percentage_probability=20) ##might want to change that prob percentage
-    except:
+                                                      output_image_path = os.getcwd(),
+                                                      custom_objects = custom_objects, minimum_percentage_probability=20) 
         logging.error("Couldn't put in bounding boxes on custom objects")
 
     try:
@@ -37,18 +35,7 @@ def detect_object(image):
     try:
          # convert list of objects to dataframe
         df = pd.DataFrame.from_records(boxes, columns = ['Object', 'Image_Name', 'Probability', 'box_ppoints']) # with probablities
-        df['Image_Name'] = "C:/Users/swatkins/Downloads/GA000000016040_2020-01-23_15h_40m.png"
-         
-        #split image_name column to get date, time and name into separated columns 
-        strdf = df['Image_Name'].str.replace('/', '_')
-        strdf = strdf.str.replace('.', '_')
-        strdf = strdf.str.split('_', expand = True) #ouput type is dataframe 
-        
-        df['Camera_ID'] = strdf[strdf.columns[4]]
-        df['Date'] = strdf[strdf.columns[5]]
-        df['Hour'] = strdf[strdf.columns[6]]
-        df['Minute'] = strdf[strdf.columns[7]]
-    #    df_count = df['Object', 'Image_name'] # without probablities
+        df_count = df['Object', 'Image_name'] # without probablities
     
     except:
         logging.error('Could not convert boxes to dataframe')
