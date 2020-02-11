df587fcf548b64975ae0daea3335019a292e0284
swatabout <sierrafouche@gmail.com>
2020-02-11 14:36:01 -0500

    Update list_files.py

--- API/list_files.py
+++ API/list_files.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-import io, boto3, logging, sqlalchemy as sql, os, numpy as np
+import io, boto3, logging, sqlalchemy as sql, os, numpy as np, glob
 from PIL import Image
 import matplotlib.image as mpim
 
-def list_files(/path/to/images/, Image_Name):
+def list_files(Image_Name):
 
+    os.chdir(/path/to/images)
+    
     filelist = []
-    for image in /path/to/images/:
+    for image in glob.glob(/path/to/images/Image_Name):
         filelist.append(image)
         
     if len(filelist) > 0 is False:
