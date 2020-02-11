65230f2a0a4fb7f27c62e6f9c2bdfaa58bea75b1
swatabout <sierrafouche@gmail.com>
2020-02-11 14:07:05 -0500

    Update app.py

--- API/app.py
+++ API/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 #from flask_sqlalchemy import SQLAlchemy
 from Jills_RetinaNet import setup_detector, pull_con, detect_objects, pushtosql
 import logging
-import config as cfg
 
 app = Flask(__name__)
 
